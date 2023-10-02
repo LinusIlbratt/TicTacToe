1a3e45e05796304744d44a276bb82f698427baf8
Linus Ilbratt <linusilbratt@gmail.com>
2023-10-02 13:23:13 +0200

    Added repeated switch case just to try getting symbols from different players

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -5,22 +6,50 @@ public class Main {
         GameBoard board = new GameBoard();
         Scanner sc = new Scanner(System.in);
 
-        System.out.println(board);
+        System.out.println("Ange namn för Player 1: ");
+        String player1Name = sc.nextLine();
+        Player player1 = new Player(player1Name, 'X');
 
-        System.out.println("Enter your placement: 1-9");
-        String playerPosition = sc.nextLine();
+        System.out.println("Ange namn för Player 2: ");
+        String player2Name = sc.nextLine();
+        Player player2 = new Player(player2Name, 'O');
+
+
+
+
+
+        System.out.println(player1.getPlayerName() + " enter your placement: 1-9");
+        String player1Position = sc.nextLine();
 
         // Enhanced switch statement (break method is included).
-        switch (playerPosition) {
-            case "1" -> board.placePlayerSymbol(0, 0, 'X');
-            case "2" -> board.placePlayerSymbol(0, 4, 'X');
-            case "3" -> board.placePlayerSymbol(0, 8, 'X');
-            case "4" -> board.placePlayerSymbol(2, 0, 'X');
-            case "5" -> board.placePlayerSymbol(2, 4, 'X');
-            case "6" -> board.placePlayerSymbol(2, 8, 'X');
-            case "7" -> board.placePlayerSymbol(4, 0, 'X');
-            case "8" -> board.placePlayerSymbol(4, 4, 'X');
-            case "9" -> board.placePlayerSymbol(4, 8, 'X');
+        switch (player1Position) {
+            case "1" -> board.placePlayerSymbol(0, 0, player1.getTicTacSymbol());
+            case "2" -> board.placePlayerSymbol(0, 4, player1.getTicTacSymbol());
+            case "3" -> board.placePlayerSymbol(0, 8, player1.getTicTacSymbol());
+            case "4" -> board.placePlayerSymbol(2, 0, player1.getTicTacSymbol());
+            case "5" -> board.placePlayerSymbol(2, 4, player1.getTicTacSymbol());
+            case "6" -> board.placePlayerSymbol(2, 8, player1.getTicTacSymbol());
+            case "7" -> board.placePlayerSymbol(4, 0, player1.getTicTacSymbol());
+            case "8" -> board.placePlayerSymbol(4, 4, player1.getTicTacSymbol());
+            case "9" -> board.placePlayerSymbol(4, 8, player1.getTicTacSymbol());
+            default -> System.out.println("Invalid input, try again");
+        }
+
+        System.out.println(board);
+
+        System.out.println(player2.getPlayerName() + " enter your placement: 1-9");
+        String player2Position = sc.nextLine();
+
+        switch (player2Position) {
+            case "1" -> board.placePlayerSymbol(0, 0, player2.getTicTacSymbol());
+            case "2" -> board.placePlayerSymbol(0, 4, player2.getTicTacSymbol());
+            case "3" -> board.placePlayerSymbol(0, 8, player2.getTicTacSymbol());
+            case "4" -> board.placePlayerSymbol(2, 0, player2.getTicTacSymbol());
+            case "5" -> board.placePlayerSymbol(2, 4, player2.getTicTacSymbol());
+            case "6" -> board.placePlayerSymbol(2, 8, player2.getTicTacSymbol());
+            case "7" -> board.placePlayerSymbol(4, 0, player2.getTicTacSymbol());
+            case "8" -> board.placePlayerSymbol(4, 4, player2.getTicTacSymbol());
+            case "9" -> board.placePlayerSymbol(4, 8, player2.getTicTacSymbol());
             default -> System.out.println("Invalid input, try again");
         }
 
