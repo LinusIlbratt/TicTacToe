a43902b7018db9592fc2f3863de04a381a97edd5
Linus Ilbratt <linusilbratt@gmail.com>
2023-10-02 11:03:59 +0200

    Added a method for player to place a symbol on the gameboard

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -3,21 +3,26 @@ public class GameBoard {
 
     // Constructor creates a 3x3 2D array with chars.
     // Each position is filled with 'space' that will represent an empty TicTacToe board.
-    public GameBoard(){
+    public GameBoard() {
         gameBoard = new char[][]{
-                {'1', ' ', '|', ' ', '2', ' ', '|', ' ', '3'},
+                {' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' '},
                 {'-', '-', '+', '-', '-', '-', '+', '-', '-'},
-                {'4', ' ', '|', ' ', '5', ' ', '|', ' ', '6'},
+                {' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' '},
                 {'-', '-', '+', '-', '-', '-', '+', '-', '-'},
-                {'7', ' ', '|', ' ', '8', ' ', '|', ' ', '9'}
+                {' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' '}
         };
+    }
+
+
+    public void placePlayerSymbol(int row, int col, char symbol) {
+        gameBoard[row][col] = symbol;
     }
 
 
     // Override the default toString method to provide a visual representation
     // of my TicTacToe game board using the StringBuilder class.
     @Override
-    public String toString(){
+    public String toString() {
         StringBuilder printBoard = new StringBuilder();
         for (char[] row : gameBoard) {
             for (char c : row) {
