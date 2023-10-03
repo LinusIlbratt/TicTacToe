0f976ff9538ce642d60838a33093f0fd448f1fbb
Linus Ilbratt <linusilbratt@gmail.com>
2023-10-03 13:29:23 +0200

    Removed GameBoard board = new GameBoard(); since this doesnt do anything anymore since we are creating a new GameBoard inside the TicTacToeGame

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        GameBoard board = new GameBoard();
         TicTacToeGame newGame = new TicTacToeGame();
         newGame.startGame();
     }
