f002064c6caaaa8bf9b193c1cf99a5ac52f94dc4
Linus Ilbratt <linusilbratt@gmail.com>
2023-10-03 11:41:09 +0200

    Added code and new methods in player

--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,8 @@ public class Player {
     private String playerName;
     private char ticTacSymbol;
 
+    private int totalWins = 0;
+
     public Player(String playerName, char ticTacSymbol) {
         this.playerName = playerName;
         this.ticTacSymbol = ticTacSymbol;
@@ -22,5 +24,23 @@ public class Player {
 
     public void setTicTacSymbol(char ticTacSymbol) {
         this.ticTacSymbol = ticTacSymbol;
+    }
+
+    public void incrementWins(){
+        totalWins++;
+    }
+
+    public int getTotalWins(){
+        return totalWins;
+    }
+
+    public String getColorCode() {
+        if (ticTacSymbol == 'X') {
+            return TextColor.ANSI_RED;
+        } else if (ticTacSymbol == 'O') {
+            return TextColor.ANSI_BLUE;
+        } else {
+            return TextColor.ANSI_RESET;  // Default color
+        }
     }
 }
