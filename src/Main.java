acaf3c8df12f1009e38d8bef49f8a30b2600bbab
Linus Ilbratt <linusilbratt@gmail.com>
2023-10-04 12:22:45 +0200

    Test to print the visuals of the dynamic boards

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        TicTacToeGame newGame = new MultiPlayerGame();
-        newGame.startGame();
+        GameBoard board3x3 = new GameBoard(3);
+        System.out.println(board3x3);
+
+        GameBoard board4x4 = new GameBoard(4);
+        System.out.println(board4x4);
+
+        GameBoard board5x5 = new GameBoard(5);
+        System.out.println(board5x5);
     }
 }
