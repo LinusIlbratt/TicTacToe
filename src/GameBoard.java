fe0d00cd3d2ff3d0d2cf0015e3646e8a20ff354c
Linus Ilbratt <linusilbratt@gmail.com>
2023-10-06 09:25:58 +0200

    Changed the gameboard so it has a dynamic size. Restructured the entire class to work properly with the dynamic board, changed some method names aswell

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -2,113 +2,125 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameBoard {
-    private ArrayList<ArrayList<Character>> gameBoard;
+    private  char[][] gameBoard;
+
     private int boardSize;
+    private int movesMade = 0; // Add a counter to the game
 
-    // Constructor that initiates an empty dynamic board
+     //Constructor creates a 3x3 2D array with chars.
+     //Each position is filled with 'space' that will represent an empty TicTacToe board.
+    public GameBoard() {
+        gameBoard = new char[][]{
+                {' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' '},
+                {'-', '-', '+', '-', '-', '-', '+', '-', '-'},
+                {' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' '},
+                {'-', '-', '+', '-', '-', '-', '+', '-', '-'},
+                {' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' '}
+        };
+    }
+
+    // Constructor that receives an int value for the board size.
     public GameBoard(int boardSize){
-        this.boardSize = boardSize;
-        this.gameBoard = new ArrayList<>();
+        int rows = 2 * boardSize - 1;
+        int cols = 3 * boardSize - 1;
+        gameBoard = new char[rows][cols];
 
-        for (int i = 0; i < boardSize; i++) {
-            ArrayList<Character> row = new ArrayList<>();
-            for (int j = 0; j < boardSize; j++) {
-                row.add(' ');
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i % 2 == 0 && j % 3 == 2) {
+                    gameBoard[i][j] = '|';
+                } else if (i % 2 == 1) {
+                    if (j == cols - 1) {
+                        gameBoard[i][j] = '-';
+                    } else {
+                        gameBoard[i][j] = (j % 3 == 2) ? '+' : '-';
+                    }
+                } else {
+                    gameBoard[i][j] = ' ';
+                }
             }
-            gameBoard.add(row);
         }
     }
 
-    public int getBoardSize() {
+    public char[][] getGameBoard() {
+        return gameBoard;
+    }
+
+    public int getBoardSize(){
         return boardSize;
     }
 
-    public boolean isEmpty(int row, int col) {
-        boolean empty = gameBoard.get(row).get(col) == ' ';
-        return empty;
-    }
 
-    public void placeMove(int row, int col, char gameSymbol) {
-        // Places a game symbol on the board if the cell is empty
-        if (isEmpty(row, col)) {
-            gameBoard.get(row).set(col, gameSymbol);
-        } else {
-            System.out.println("Could not place " + gameSymbol);
+
+    //Method for the placement of the TicTac Symbols and to check to see if a position is free to place the player symbol.
+    public boolean placePlayerSymbol(int row, int col, char symbol) {
+        if (row < 0 || row > 4 || col < 0 || col > 8 || gameBoard[row][col] != ' ') {
+            return false;
         }
+        gameBoard[row][col] = symbol;
+        movesMade++; // Increase the counter each time a move is made
+        return true;
     }
 
-    public boolean isFull() {
-        // Check to see if the board is full
-        for (int i = 0; i < boardSize; i++) {
-            for (int j = 0; j < boardSize; j++) {
-                if (isEmpty(i, j)) {
-                    return false;
+    public List<Integer> getAvailableMoves() {
+        List<Integer> availableMoves = new ArrayList<>();
+        char[][] board = getGameBoard(); // Making a copy of the current board and not a new board.
+        // Makes it easier to work with, so we don't have to use a get method each time we want to make a reference.
+        int boardSize = board.length; // Getting total amounts of rows (not columns)
+
+        for (int row = 0; row < boardSize; row++){
+            for (int col = 0; col < boardSize; col++){
+                if(board[row][col] == ' ') {
+                    int positionsLeft = row * boardSize + col + 1; // Converts our 2D array to 1D positions with a 1-base index. (1, 2, 3 etc)
+                    availableMoves.add(positionsLeft);
                 }
             }
         }
-        return true;
+        return availableMoves;
     }
 
-    public boolean hasWinner() {
-        // Checking rows and columns
-        for (int i = 0; i < boardSize; i++) {
-            if (isWinningLine(0, i, 1, 0) || // Columns
-            isWinningLine(i, 0, 0, 1)) {     // Rows
-                return true;
-            }
-        }
-        // Checking the crosses (diagonals)
-        return isWinningLine(0, 0, 1, 1) ||  // First diagonal
-                isWinningLine(0, boardSize - 1, 1, -1); // Second diagonal
+    public boolean isBoardFull() {
+        return movesMade >= 9;
     }
 
-    private boolean isWinningLine(int startX, int startY, int stepX, int stepY) {
-        char gameSymbol = gameBoard.get(startX).get(startY);
-        if (gameSymbol == ' ') return false;
-
-        for (int i = 1; i < boardSize; i++) {
-            if (gameBoard.get(startX + i * stepX).get(startY + i * stepY) != gameSymbol) {
-                return false;
-            }
-        }
-        return true;
+    public boolean winningCondition(char symbol) {
+        return  // Horizontal lines
+                (gameBoard[0][0] == symbol && gameBoard[0][4] == symbol && gameBoard[0][8] == symbol) ||
+                (gameBoard[2][0] == symbol && gameBoard[2][4] == symbol && gameBoard[2][8] == symbol) ||
+                (gameBoard[4][0] == symbol && gameBoard[4][4] == symbol && gameBoard[4][8] == symbol) ||
+                // Vertical lines
+                (gameBoard[0][0] == symbol && gameBoard[2][0] == symbol && gameBoard[4][0] == symbol) ||
+                (gameBoard[0][4] == symbol && gameBoard[2][4] == symbol && gameBoard[4][4] == symbol) ||
+                (gameBoard[0][8] == symbol && gameBoard[2][8] == symbol && gameBoard[4][8] == symbol) ||
+                // Cross lines
+                (gameBoard[0][0] == symbol && gameBoard[2][4] == symbol && gameBoard[4][8] == symbol) ||
+                (gameBoard[0][8] == symbol && gameBoard[2][4] == symbol && gameBoard[4][0] == symbol);
     }
 
+    public void resetGameBoard(){
+        gameBoard = new char[][]{
+                {' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' '},
+                {'-', '-', '+', '-', '-', '-', '+', '-', '-'},
+                {' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' '},
+                {'-', '-', '+', '-', '-', '-', '+', '-', '-'},
+                {' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' '}
+        };
+        movesMade = 0;
+
+    }
+
+
+     //Override the default toString method to provide a visual representation
+     // of my TicTacToe game board using the StringBuilder class.
     @Override
     public String toString() {
         StringBuilder printBoard = new StringBuilder();
-
-        // Skriv ut kolumnbokstäverna överst
-        printBoard.append("  ");
-        for (int col = 0; col < boardSize; col++) {
-            char colLetter = (char) ('A' + col);
-            printBoard.append(" ").append(colLetter).append(" ");
-        }
-        printBoard.append('\n');
-
-
-        for (int i = 0; i < boardSize; i++) {
-            // Print row numbers
-            printBoard.append(String.format("%2d ", i + 1));
-            for (int j = 0; j < boardSize; j++) {
-                printBoard.append(gameBoard.get(i).get(j));
-                if (j < boardSize - 1) {
-                    printBoard.append(" |");
-                }
+        for (char[] row : gameBoard) {
+            for (char c : row) {
+                printBoard.append(c);
             }
             printBoard.append('\n');  // New line after each row
-
-            // Print horizontal separators except after the last row
-            if (i < boardSize - 1) {
-                printBoard.append("   ");  // Extra space for the row number
-                for (int j = 0; j < boardSize - 1; j++) {
-                    printBoard.append("--+");
-                }
-                printBoard.append("--\n");
-            }
         }
         return printBoard.toString();
     }
-
-
 }
