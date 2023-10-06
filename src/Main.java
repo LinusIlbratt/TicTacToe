345d009fba2ede33168f20c36744930cf4c320f7
Linus Ilbratt <linusilbratt@gmail.com>
2023-10-06 08:34:34 +0200

    removed the test boards and added methods to start the game

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        GameBoard board3x3 = new GameBoard(3);
-        System.out.println(board3x3);
+        GameManager gameManager = new GameManager();
 
-        GameBoard board4x4 = new GameBoard(4);
-        System.out.println(board4x4);
+        gameManager.displayMenu();
 
-        GameBoard board5x5 = new GameBoard(5);
-        System.out.println(board5x5);
+        gameManager.startGame();
+
     }
+
 }
