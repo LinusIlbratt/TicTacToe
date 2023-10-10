e2efb524b51dde91650b4ec1a677207e86d24ae6
Linus Ilbratt <linusilbratt@gmail.com>
2023-10-10 13:21:47 +0200

    Changed position of the methods and did a reformat code

--- src/GameManager.java
+++ src/GameManager.java
@@ -19,124 +19,6 @@ public class GameManager {
         this.currentPlayer = null;
     }
 
-    public void switchPlayer() {
-        if (currentPlayer == player1) {
-            currentPlayer = player2;
-        } else {
-            currentPlayer = player1;
-        }
-    }
-
-    public void initializeSinglePlayer() {
-        this.player1 = HumanPlayer.createHumanPlayer('X', sc);
-        this.player2 = new ComputerPlayer("Computer", 'O');
-        this.startingPlayer = player1;
-        this.currentPlayer = startingPlayer;
-    }
-
-    public void initializeMultiplayer(){
-        this.player1 = HumanPlayer.createHumanPlayer('X', sc);
-        this.player2 = HumanPlayer.createHumanPlayer('O', sc);
-        this.startingPlayer = player1;
-        this.currentPlayer = startingPlayer;
-    }
-
-    public void displayTotalWins() {
-        System.out.println("Current score: ");
-        System.out.println(player1.getPlayerName() + ": " + player1.getTotalWins() + " wins.");
-        System.out.println(player2.getPlayerName() + ": " + player2.getTotalWins() + " wins.");
-    }
-
-    public void gameLoop() {
-        boolean gameIsRunning = true;
-        while (gameIsRunning) {
-            // Display the board
-            System.out.println(gameBoard);
-
-            // Let the current player make a move by calling the makeMove function
-            System.out.println("Current player is: " + currentPlayer.getPlayerName());
-            currentPlayer.makeMove(gameBoard);
-
-            // Check if a player has won or if the board is full, then it's a tie
-            if (gameBoard.checkWinner()) {
-                System.out.println(currentPlayer.getPlayerName() + " has won!\n");
-                currentPlayer.incrementWins();
-                gameIsRunning = false;
-            } else if (gameBoard.isGameBoardFull()) {
-                System.out.println("It's a tie!");
-                gameIsRunning = false;
-            } else {
-                // Switching players by calling the switchPlayer method
-                switchPlayer();
-            }
-        }
-    }
-
-    public void startGame() {
-        String playAgain;
-        do {
-            gameLoop();
-            displayTotalWins();
-            System.out.println("Do you want to play again? (y/n)");
-            System.out.print("> ");
-
-            playAgain = sc.nextLine().trim();
-            if (playAgain.equalsIgnoreCase("y")) {
-                if (startingPlayer == player1) {
-                    startingPlayer = player2;
-                } else {
-                    startingPlayer = player1;
-                }
-                currentPlayer = startingPlayer; // Set the currentPlayer to the new starting player
-                gameBoard.resetGameBoard();
-            }
-        } while (playAgain.equalsIgnoreCase("y"));
-
-    }
-
-    public void chooseGameBoardSize(){
-        boolean invalidUserChoice;
-
-        do {
-            invalidUserChoice = false;
-            System.out.println("Select your board size.");
-            System.out.println("3. 3x3");
-            System.out.println("4. 4x4");
-            System.out.println("5. 5x5");
-            System.out.print("> ");
-
-            try {
-                int userBoardChoice = sc.nextInt();
-                sc.nextLine(); // Clear the buffer
-
-                if (userBoardChoice >= 3 && userBoardChoice <= 5) {
-                    gameBoard = new GameBoard(userBoardChoice);
-                } else {
-                    invalidUserChoice = true;
-                }
-            } catch (InputMismatchException e) {
-                sc.nextLine(); // Clear the buffer
-                invalidUserChoice = true;
-            }
-
-            if (invalidUserChoice) {
-                System.out.println("Invalid choice. Please try again.");
-            }
-        } while (invalidUserChoice);
-    }
-
-    public void displayRules(){
-        System.out.println("""
-                Rules:
-                               
-                The game is played on a grid that's 3x3 (or large if chosen).
-                Player 1 is X and Player 2 is 0.
-                Players take turns putting their symbols on the grid.
-                The first player to get 3 (or the board size) of their symbols in a row (up, down, across, or diagonally) wins.
-                If all the cells on the grid are filled and no player has their symbols in a row, then the game is a tie.
-                """);
-    }
-
     public void gameMenu() {
         System.out.println("Welcome to TicTacToe!\n");
 
@@ -173,6 +55,123 @@ public class GameManager {
         chooseGameBoardSize();
     }
 
+    public void startGame() {
+        String playAgain;
+        do {
+            gameLoop();
+            displayTotalWins();
+            System.out.println("Do you want to play again? (y/n)");
+            System.out.print("> ");
+
+            playAgain = sc.nextLine().trim();
+            if (playAgain.equalsIgnoreCase("y")) {
+                if (startingPlayer == player1) {
+                    startingPlayer = player2;
+                } else {
+                    startingPlayer = player1;
+                }
+                currentPlayer = startingPlayer; // Set the currentPlayer to the new starting player
+                gameBoard.resetGameBoard();
+            }
+        } while (playAgain.equalsIgnoreCase("y"));
+
+    }
+
+    public void gameLoop() {
+        boolean gameIsRunning = true;
+        while (gameIsRunning) {
+            // Display the board
+            System.out.println(gameBoard);
+
+            // Let the current player make a move by calling the makeMove function
+            System.out.println("Current player is: " + currentPlayer.getPlayerName());
+            currentPlayer.makeMove(gameBoard);
+
+            // Check if a player has won or if the board is full, then it's a tie
+            if (gameBoard.checkWinner()) {
+                System.out.println(currentPlayer.getPlayerName() + " has won!\n");
+                currentPlayer.incrementWins();
+                gameIsRunning = false;
+            } else if (gameBoard.isGameBoardFull()) {
+                System.out.println("It's a tie!");
+                gameIsRunning = false;
+            } else {
+                // Switching players by calling the switchPlayer method
+                switchPlayer();
+            }
+        }
+    }
+
+    public void displayRules() {
+        System.out.println("""
+                Rules:
+                               
+                The game is played on a grid that's 3x3 (or large if chosen).
+                Player 1 is X and Player 2 is 0.
+                Players take turns putting their symbols on the grid.
+                The first player to get 3 (or the board size) of their symbols in a row (up, down, across, or diagonally) wins.
+                If all the cells on the grid are filled and no player has their symbols in a row, then the game is a tie.
+                """);
+    }
+
+    public void chooseGameBoardSize() {
+        boolean invalidUserChoice;
+
+        do {
+            invalidUserChoice = false;
+            System.out.println("Select your board size.");
+            System.out.println("3. 3x3");
+            System.out.println("4. 4x4");
+            System.out.println("5. 5x5");
+            System.out.print("> ");
+
+            try {
+                int userBoardChoice = sc.nextInt();
+                sc.nextLine(); // Clear the buffer
+
+                if (userBoardChoice >= 3 && userBoardChoice <= 5) {
+                    gameBoard = new GameBoard(userBoardChoice);
+                } else {
+                    invalidUserChoice = true;
+                }
+            } catch (InputMismatchException e) {
+                sc.nextLine(); // Clear the buffer
+                invalidUserChoice = true;
+            }
+
+            if (invalidUserChoice) {
+                System.out.println("Invalid choice. Please try again.");
+            }
+        } while (invalidUserChoice);
+    }
+
+    public void initializeSinglePlayer() {
+        this.player1 = HumanPlayer.createHumanPlayer('X', sc);
+        this.player2 = new ComputerPlayer("Computer", 'O');
+        this.startingPlayer = player1;
+        this.currentPlayer = startingPlayer;
+    }
+
+    public void initializeMultiplayer() {
+        this.player1 = HumanPlayer.createHumanPlayer('X', sc);
+        this.player2 = HumanPlayer.createHumanPlayer('O', sc);
+        this.startingPlayer = player1;
+        this.currentPlayer = startingPlayer;
+    }
+
+    public void switchPlayer() {
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
+    }
+
+    public void displayTotalWins() {
+        System.out.println("Current score: ");
+        System.out.println(player1.getPlayerName() + ": " + player1.getTotalWins() + " wins.");
+        System.out.println(player2.getPlayerName() + ": " + player2.getTotalWins() + " wins.");
+    }
 
 }
 
