b99cf24c508924b63cd333fea5693cbaa879ebef
Linus Ilbratt <linusilbratt@gmail.com>
2023-10-13 11:45:08 +0200

    Removed start game method

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         GameManager gameManager = new GameManager();
-
         gameManager.gameMenu();
-
-        gameManager.startGame();
 
     }
 
